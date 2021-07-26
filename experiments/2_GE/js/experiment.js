c2768b175ce048ecb3e1b693eab38b2468acedd2
alexiah53 <alexiah@stanford.edu>
2021-07-25 20:23:11 -0400

    updated end bit

--- experiments/2_GE/js/experiment.js
+++ experiments/2_GE/js/experiment.js
@@ -354,51 +354,289 @@ prime_aud.onended = function() {
     },
   });
 
-  slides.follow_up = slide({
-  	  name: "follow_up",
+// WORDS
+   slides.words = slide({
+   	  name: "words",
+       start: function() {
+ 		  exp.allow_key_press = 0;
+ 		  console.log("exp.allow_key_press", exp.allow_key_press)
+ 		  $(".err").hide();  
+		  
+		  $("#words_accentedness_speaker1").hide();
+		  $(".words_race_speaker1").hide();
+
+		  $("#words_speaker_1_reaction_1").hide();
+		  $("#words_speaker_1_reaction_2").hide();
+		  $("#words_speaker_1_reaction_3").hide();
+
+		  $("#words_accentedness_speaker2").hide();
+		  $(".words_race_speaker2").hide();
+
+		  $("#words_speaker_2_reaction_1").hide();
+		  $("#words_speaker_2_reaction_2").hide();
+		  $("#words_speaker_2_reaction_3").hide();
+		
+		var words_race_speaker1_aud = document.getElementById("speaker_1_2A_words");  	
+		words_race_speaker1_aud.onended = function() {
+			
+			  $("#words_accentedness_speaker1").show();
+			  $(".words_race_speaker1").show();
+
+			  $("#words_speaker_1_reaction_1").show();
+			  $("#words_speaker_1_reaction_2").show();
+			  $("#words_speaker_1_reaction_3").show();
+
+		  }
+		  
+  		var words_race_speaker2_aud = document.getElementById("speaker_2_2A_words");  	
+  		words_race_speaker2_aud.onended = function() {
+
+
+		  $("#words_accentedness_speaker2").show();
+		  $(".words_race_speaker2").show();
+
+		  $("#words_speaker_2_reaction_1").show();
+		  $("#words_speaker_2_reaction_2").show();
+		  $("#words_speaker_2_reaction_3").show();
+	  }
+	  
+       },	   
+
+       // handle click on "Continue" button
+       button_follow_up: function() {
+		  
+ 		  var check_words_race1 = document.querySelectorAll('[name="words_race_speaker1"]:checked');
+ 		  var check_words_race2 = document.querySelectorAll('[name="words_race_speaker2"]:checked');
+
+
+ 		  if  (!$("#words_speaker_1_reaction_1").val() |
+ 			  !$("#words_speaker_1_reaction_2").val() |
+ 			  !$("#words_speaker_1_reaction_3").val() |
+ 			  !$("#words_speaker_2_reaction_1").val() |
+ 			  !$("#words_speaker_2_reaction_2").val() |
+ 			  !$("#words_speaker_2_reaction_3").val() |
+ 			  !$("#words_accentedness_speaker1").val() |
+ 		  !$("#words_accentedness_speaker2").val() |
+ 			  check_words_race1.length < 1 |
+ 		  check_words_race2.length < 1 ) {
+
+ 		  	$(".err").show();
+ 		}
+ 			// else {
+ 			// 	            this.log_responses();
+ 			// 	            exp.go(); //use exp.go() if and only if there is no "present"ed data, ie no list of stimuli.
+ 			// }
+ 			//       },
+	  
+	  
+ 			else {
+				
+ 				var words_race_speaker1 = document.querySelectorAll('[name="words_race_speaker1"]:checked');
+ 				exp.words_race_speaker1_list = [];
+ 				for (var i = 0; i < words_race_speaker1.length; i++) {
+ 					if (words_race_speaker1[i].type=="checkbox" && words_race_speaker1[i].checked == true){
+ 						exp.words_race_speaker1_list += words_race_speaker1[i].value+", \n";
+ 					}
+ 				}
+					
+ 				var words_race_speaker2 = document.querySelectorAll('[name="words_race_speaker2"]:checked');
+ 				exp.words_race_speaker2_list = [];
+ 				for (var i = 0; i < words_race_speaker2.length; i++) {
+ 					if (words_race_speaker2[i].type=="checkbox" && words_race_speaker2[i].checked == true){
+ 							exp.words_race_speaker2_list += words_race_speaker2[i].value+", \n";
+ 						}		
+ 				}
+ 				this.log_responses();
+ 				exp.go();
+ 			}
+			
+ 				},
+	  
+       // save response
+       log_responses: function() {
+         exp.data_trials.push({
+      	  	
+ 			words_speaker_1_accentedness: $("#words_accentedness_speaker1").val(),
+ 			words_speaker_2_accentedness:$("#words_accentedness_speaker2").val(),
+ 			words_speaker_1_reaction_1:$("#words_speaker_1_reaction_1").val(),
+ 			words_speaker_1_reaction_2:$("#words_speaker_1_reaction_2").val(),
+ 			words_speaker_1_reaction_3:$("#words_speaker_1_reaction_3").val(),
+ 			words_speaker_2_reaction_1:$("#words_speaker_2_reaction_1").val(),
+ 			words_speaker_2_reaction_2:$("#words_speaker_2_reaction_2").val(),
+   			words_speaker_2_reaction_3:$("#words_speaker_2_reaction_3").val(),
+ 			speaker_1_race_words: exp.words_race_speaker1_list,
+ 			speaker_2_race_words: exp.words_race_speaker2_list
+			
+         });
+   	  }
+   });
+   
+   
+   
+
+// SENTENCES SLIDE
+  slides.sentences = slide({
+  	  name: "sentences",
       start: function() {
 		  exp.allow_key_press = 0;
 		  console.log("exp.allow_key_press", exp.allow_key_press)
+		  $(".err").hide();
+		  
+		  $("#sentences_accentedness_speaker1").hide();
+		  $(".sentences_race_speaker1").hide();
+
+		  $("#sentences_speaker_1_reaction_1").hide();
+		  $("#sentences_speaker_1_reaction_2").hide();
+		  $("#sentences_speaker_1_reaction_3").hide();
+
+		  $("#sentences_accentedness_speaker2").hide();
+		  $(".sentences_race_speaker2").hide();
+
+		  $("#sentences_speaker_2_reaction_1").hide();
+		  $("#sentences_speaker_2_reaction_2").hide();
+		  $("#sentences_speaker_2_reaction_3").hide();
+		
+		var words_race_speaker1_aud = document.getElementById("speaker_sentence_1");  	
+		words_race_speaker1_aud.onended = function() {
+			
+			  $("#sentences_accentedness_speaker1").show();
+			  $(".sentences_race_speaker1").show();
+
+			  $("#sentences_speaker_1_reaction_1").show();
+			  $("#sentences_speaker_1_reaction_2").show();
+			  $("#sentences_speaker_1_reaction_3").show();
+
+		  }
+		  
+  		var words_race_speaker2_aud = document.getElementById("speaker_sentence_2");  	
+  		words_race_speaker2_aud.onended = function() {
+
+		  $("#sentences_accentedness_speaker2").show();
+		  $(".sentences_race_speaker2").show();
+
+		  $("#sentences_speaker_2_reaction_1").show();
+		  $("#sentences_speaker_2_reaction_2").show();
+		  $("#sentences_speaker_2_reaction_3").show();
+	  }
       },
 
       // handle click on "Continue" button
       button_follow_up: function() {
+		  
+		  var check_sentences_race1 = document.querySelectorAll('[name="sentences_race_speaker1"]:checked');
+		  var check_sentences_race2 = document.querySelectorAll('[name="sentences_race_speaker2"]:checked');
 
-		  if  (!$("#speaker_1_reaction_1").val() |
-			  !$("#speaker_1_reaction_2").val() |
-			  !$("#speaker_1_reaction_3").val() |
-		  	  !$("#speaker_1_reaction_4").val() |
-			  !$("#speaker_1_reaction_5").val() |
-			  !$("#speaker_2_reaction_1").val() |
-			  !$("#speaker_2_reaction_2").val() |
-			  !$("#speaker_2_reaction_3").val() |
-		  	  !$("#speaker_2_reaction_4").val() |
-		 	  !$("#speaker_2_reaction_5").val()) {
+
+		  if  (!$("#sentences_speaker_1_reaction_1").val() |
+			  !$("#sentences_speaker_1_reaction_2").val() |
+			  !$("#sentences_speaker_1_reaction_3").val() |
+			  !$("#sentences_speaker_2_reaction_1").val() |
+			  !$("#sentences_speaker_2_reaction_2").val() |
+			  !$("#sentences_speaker_2_reaction_3").val() |
+			  !$("#sentences_accentedness_speaker1").val() |
+		  !$("#sentences_accentedness_speaker2").val() |
+			  check_sentences_race1.length < 1 |
+		  check_sentences_race2.length < 1 ) {
 
 		  	$(".err").show();
 		}
+			// else {
+			// 	            this.log_responses();
+			// 	            exp.go(); //use exp.go() if and only if there is no "present"ed data, ie no list of stimuli.
+			// }
+			//       },
+	  
+	  
 			else {
-	            this.log_responses();
-	            exp.go(); //use exp.go() if and only if there is no "present"ed data, ie no list of stimuli.
+				
+				var sentences_race_speaker1 = document.querySelectorAll('[name="sentences_race_speaker1"]:checked');
+				exp.sentences_race_speaker1_list = [];
+				for (var i = 0; i < sentences_race_speaker1.length; i++) {
+					if (sentences_race_speaker1[i].type=="checkbox" && sentences_race_speaker1[i].checked == true){
+						exp.sentences_race_speaker1_list += sentences_race_speaker1[i].value+", \n";
+					}
+				}
+					
+				var sentences_race_speaker2 = document.querySelectorAll('[name="sentences_race_speaker2"]:checked');
+				exp.sentences_race_speaker2_list = [];
+				for (var i = 0; i < sentences_race_speaker2.length; i++) {
+					if (sentences_race_speaker2[i].type=="checkbox" && sentences_race_speaker2[i].checked == true){
+							exp.sentences_race_speaker2_list += sentences_race_speaker2[i].value+", \n";
+						}		
+				}
+				this.log_responses();
+				exp.go();
 			}
-      },
-
+			
+				},
+	  
       // save response
       log_responses: function() {
         exp.data_trials.push({
-      	  	"speaker_1_reaction_1":$("#speaker_1_reaction_1").val(),
-			"speaker_1_reaction_2":$("#speaker_1_reaction_2").val(),
-			"speaker_1_reaction_3":$("#speaker_1_reaction_3").val(),
-			"speaker_1_reaction_4":$("#speaker_1_reaction_4").val(),
-			"speaker_1_reaction_5":$("#speaker_1_reaction_5").val(),
-			"speaker_2_reaction_1":$("#speaker_2_reaction_1").val(),
-			"speaker_2_reaction_2":$("#speaker_2_reaction_2").val(),
-  			"speaker_2_reaction_3":$("#speaker_2_reaction_3").val(),
-			"speaker_2_reaction_4":$("#speaker_2_reaction_4").val(),
-			"speaker_2_reaction_5":$("#speaker_2_reaction_5").val(),
+      	  	
+			sentences_speaker_1_accentedness: $("#sentences_accentedness_speaker1").val(),
+			sentences_speaker_2_accentedness:$("#sentences_accentedness_speaker2").val(),
+			sentences_speaker_1_reaction_1:$("#sentences_speaker_1_reaction_1").val(),
+			sentences_speaker_1_reaction_2:$("#sentences_speaker_1_reaction_2").val(),
+			sentences_speaker_1_reaction_3:$("#sentences_speaker_1_reaction_3").val(),
+			sentences_speaker_2_reaction_1:$("#sentences_speaker_2_reaction_1").val(),
+			sentences_speaker_2_reaction_2:$("#sentences_speaker_2_reaction_2").val(),
+  			sentences_speaker_2_reaction_3:$("#sentences_speaker_2_reaction_3").val(),
+			speaker_1_race_sentences: exp.sentences_race_speaker1_list,
+			speaker_2_race_sentences: exp.sentences_race_speaker2_list
+			
         });
   	  }
   });
+
+
+
+  // slides.follow_up = slide({
+//   	  name: "follow_up",
+//       start: function() {
+// 		  exp.allow_key_press = 0;
+// 		  console.log("exp.allow_key_press", exp.allow_key_press)
+//       },
+//
+//       // handle click on "Continue" button
+//       button_follow_up: function() {
+//
+// 		  if  (!$("#speaker_1_reaction_1").val() |
+// 			  !$("#speaker_1_reaction_2").val() |
+// 			  !$("#speaker_1_reaction_3").val() |
+// 		  	  !$("#speaker_1_reaction_4").val() |
+// 			  !$("#speaker_1_reaction_5").val() |
+// 			  !$("#speaker_2_reaction_1").val() |
+// 			  !$("#speaker_2_reaction_2").val() |
+// 			  !$("#speaker_2_reaction_3").val() |
+// 		  	  !$("#speaker_2_reaction_4").val() |
+// 		 	  !$("#speaker_2_reaction_5").val()) {
+//
+// 		  	$(".err").show();
+// 		}
+// 			else {
+// 	            this.log_responses();
+// 	            exp.go(); //use exp.go() if and only if there is no "present"ed data, ie no list of stimuli.
+// 			}
+//       },
+//
+//       // save response
+//       log_responses: function() {
+//         exp.data_trials.push({
+//       	  	"speaker_1_reaction_1":$("#speaker_1_reaction_1").val(),
+// 			"speaker_1_reaction_2":$("#speaker_1_reaction_2").val(),
+// 			"speaker_1_reaction_3":$("#speaker_1_reaction_3").val(),
+// 			"speaker_1_reaction_4":$("#speaker_1_reaction_4").val(),
+// 			"speaker_1_reaction_5":$("#speaker_1_reaction_5").val(),
+// 			"speaker_2_reaction_1":$("#speaker_2_reaction_1").val(),
+// 			"speaker_2_reaction_2":$("#speaker_2_reaction_2").val(),
+//   			"speaker_2_reaction_3":$("#speaker_2_reaction_3").val(),
+// 			"speaker_2_reaction_4":$("#speaker_2_reaction_4").val(),
+// 			"speaker_2_reaction_5":$("#speaker_2_reaction_5").val(),
+//         });
+//   	  }
+//   });
+
 
   // slide to collect subject information
   slides.subj_info = slide({
@@ -525,7 +763,8 @@ function init() {
 	"practice_trial_2",
     "startExp",
     "trial",
-	"follow_up",
+	"words",
+	"sentences",
     "subj_info",
     "thanks"
   ];
